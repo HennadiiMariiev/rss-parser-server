d834d72b278e6801a20bf141db29f5817a105a64
Hennadii_Mariiev <marieff1990@gmail.com>
2022-07-31 22:15:13 +0300

    fix duplicate error on update post

--- src/controllers/posts.controllers/updatePost.controller.js
+++ src/controllers/posts.controllers/updatePost.controller.js
@@ -1,3 +1,6 @@
+const { BadRequest } = require('http-errors');
+
+const { isDuplicateKeyError } = require('../../helpers/service.helpers');
 const { PostsService } = require('../../services');
 const { isErrorOrFalsyValue } = require('../../helpers/controller.helpers');
 
@@ -5,7 +8,8 @@ const updatePost = async (req, res, next) => {
   const post = await PostsService.updatePost(req.params, req.body);
 
   if (isErrorOrFalsyValue(post)) {
-    return next(post);
+    const error = post;
+    return isDuplicateKeyError(error) ? next(new BadRequest('Post with this title has already existed.')) : next(error);
   }
 
   res.status(200).json({ message: 'success', data: post });
